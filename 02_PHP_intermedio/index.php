e21d859ec96493a327c896ccd9f8b5fc1da0018b
Jeremy <jere.deleon8520@gmail.com>
2025-04-05 22:09:17 -0600

    Funcion de reenderizado

--- 02_PHP_intermedio/index.php
+++ 02_PHP_intermedio/index.php
@@ -7,6 +7,6 @@
     $untilMessage = get_until_message($data["days_until"]);
 ?>
 
-<?php require "sections/head.php"; ?>
+<?php render_template('head', $data); ?>
 <?php require "sections/main.php"; ?>
 <?php require "sections/styles.php" ?>
