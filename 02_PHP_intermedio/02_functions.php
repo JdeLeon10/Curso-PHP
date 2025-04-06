9d2cd3910d848292e49d79a2ce5d0e532a19beab
Jeremy <jere.deleon8520@gmail.com>
2025-04-05 22:09:31 -0600

    Creacion de nueva funcion para reenderizado en index.php

--- 02_PHP_intermedio/02_functions.php
+++ 02_PHP_intermedio/02_functions.php
@@ -1,5 +1,10 @@
 <?php declare(strict_types=1);
     
+    function render_template(string $template, array $data = []){
+        extract ($data); // Extrae las variables del array $data
+        require "templates/$template.php";
+    }
+
     function get_data(string $url): array {
         $result = file_get_contents($url);
         $data = json_decode($result, true);
